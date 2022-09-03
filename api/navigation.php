e926d480317e5f256113cab8a04da95f27012f38
Kai <git@kai-thater.de>
2022-09-03 15:53:50 +0200

    navigation_api

---
+++ api/navigation.php
@@ -0,0 +1,39 @@
+<?php
+
+  include("../admin/include/functions.php");
+  include("../admin/include/db_connect.php");
+  include("../admin/include/db_querys.php");
+  include("../admin/include/times.php");
+
+  $return_array = get_childs(0);
+
+    
+
+    function get_childs($navigation_id){
+        $where = array();
+        $wh['col'] = "navigation_parent";
+        $wh['typ'] = "=";
+        $wh['val'] = $navigation_id;
+        array_push($where, $wh);
+
+        $order = array();
+        $or['col'] = "navigation_order";
+        $or['dir'] = "ASC";
+        array_push($order, $or);
+        
+        $db_array = db_select("navigation", $where, $order);
+        $local_array = array();
+        foreach($db_array as $line){
+            $childs = get_childs($line['navigation_id']);
+            if(count($childs)> 0){
+               $line['childs'] = $childs;
+            }
+            array_push($local_array, $line);
+        }
+        return $local_array;
+
+    }
+    header('Content-Type: application/json');
+    echo json_encode($return_array);
+
+  ?>
