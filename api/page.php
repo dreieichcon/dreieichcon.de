5af8e8a56fe8ba31f619a5884d9062232173477d
Kai <git@kai-thater.de>
2022-09-03 15:58:12 +0200

    page_api

---
+++ api/page.php
@@ -0,0 +1,32 @@
+<?php
+if(!isset($_GET['page_id'])){
+    http_response_code(400);
+    die;
+}
+
+  include("../admin/include/functions.php");
+  include("../admin/include/db_connect.php");
+  include("../admin/include/db_querys.php");
+  include("../admin/include/times.php");
+
+  $return_array = array();
+  $where = array();
+  $wh['col'] = "page_id";
+  $wh['typ'] = "=";
+  $wh['val'] = $_GET['page_id'];
+  array_push($where, $wh);
+
+  $db_array = db_select("page", $where);
+
+    if(count($db_array)>0){
+    $return_array = $db_array[0];
+    }else{
+    http_response_code(404);
+    die;
+    }
+    
+    $return_array['page_content']= array();
+    header('Content-Type: application/json');
+    echo json_encode($return_array);
+
+  ?>
