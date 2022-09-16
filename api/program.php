974fe9a2a282283096d80f0b7ae4e4d494501db2
Kai <git@kai-thater.de>
2022-09-16 12:12:11 +0200

    Program_API

---
+++ api/program.php
@@ -0,0 +1,41 @@
+<?php
+
+//check information
+//   if(!isset($_GET['page_id'])){
+//       http_response_code(400);
+//       die;
+//   }
+
+//   $page_id = $_GET['page_id'];
+
+//load librarys
+  include("../admin/include/functions.php");
+  include("../admin/include/db_connect.php");
+  include("../admin/include/db_querys.php");
+  include("../admin/include/times.php");
+
+  $return_array = array();
+
+$sql		= "SELECT * FROM program p, program_type t, location l WHERE p.location_id = l.location_id AND p.program_type_id = t.program_type_id ORDER BY p.program_start_ts ASC, l.location_name_de ASC";
+                                                        
+$pdo 		= new PDO($pdo_mysql, $pdo_db_user, $pdo_db_pwd);
+
+$statement	= $pdo->prepare($sql);
+
+//$statement->bindParam(':page_id', $page_id);
+
+$statement->execute();
+
+$blog = array();
+
+while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+    foreach ($row as $key => $value){
+        $row[$key] = db_parse($value);
+    }
+    array_push($return_array, $row);
+}
+    
+header('Content-Type: application/json');
+  echo json_encode($return_array);
+
+?>
