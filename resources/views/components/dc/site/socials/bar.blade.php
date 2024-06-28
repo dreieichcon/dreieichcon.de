6211757efccec29b57873c0302875dc7269aec81
Kai <git@kai-thater.de>
2024-06-28 15:20:36 +0200

    first components

---
+++ resources/views/components/dc/site/socials/bar.blade.php
@@ -0,0 +1,10 @@
+<div class="socials-stack">
+<div class="socials-bar">
+    {{--This is one socials icon--}}
+    @for($i=0; $i<10; $i++)
+        <x-dc.site.socials.item />
+    @endfor
+
+
+</div>
+</div>
