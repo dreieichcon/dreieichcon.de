b82e67e2502c629dd664bf652c39088f909ef617
Kai <git@kai-thater.de>
2024-07-12 16:10:37 +0200

    adding datatables and fontawesome 6

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -40,6 +40,18 @@
     {{-- Extra Configured Plugins Stylesheets --}}
     @include('adminlte::plugins', ['type' => 'css'])
 
+{{--    Custom Plugins--}}
+    <!-- jQuery -->
+    <script src="{{ asset("/assets/vendor/jquery/js/jquery-3.7.1.min.js") }}"></script>
+
+    <!-- FontAwesome -->
+    <link href="{{ asset("/assets/vendor/fontawesome-free-6.5.2-web/css/all.css") }}" rel="stylesheet">
+
+    <!-- DataTables -->
+    <link href="{{ asset("/assets/vendor/DataTables/datatables.min.css") }}" rel="stylesheet">
+    <script src="{{ asset("/assets/vendor/DataTables/datatables.min.js") }}"></script>
+
+
     {{-- Livewire Styles --}}
     @if(config('adminlte.livewire'))
         @if(intval(app()->version()) >= 7)
@@ -84,7 +96,7 @@
 
     {{-- Base Scripts --}}
     @if(!config('adminlte.enabled_laravel_mix'))
-        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+{{--        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>--}}
         <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
         <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
@@ -107,6 +119,8 @@
     {{-- Custom Scripts --}}
     @yield('adminlte_js')
 
+    <!-- custom js -->
+    @vite("resources/js/custom.js")
 </body>
 
 </html>
