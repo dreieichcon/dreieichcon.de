379aba04161e1c0fd66d104bf4819b963bf70d75
Demolite <tobi.brinke@gmail.com>
2024-10-31 11:46:50 +0100

    Fixed Error Page Image Size

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,46 +1,19 @@
 @extends("layouts.main")
 
-
-
 @section("content")
-    <div style="height:2rem">&nbsp;</div>
-    <div
-        class="mt-4"
-
-        style="
-        /*border: 1px solid white;*/
-        height:50vH;
-        display: flex;
-        justify-content: space-around;
-        align-content: space-around;
-        align-items: center;
-        flex-direction: row;
-        ">
-        <div style="
-        /*border: 1px solid green;*/
-        display: flex;
-        flex-direction: column;
-        width:100%;
-        justify-content: space-around;
-        align-content: space-around;
-        align-items: center;
-        ">
+    <div class="error-wrapper">
+        <div class="error-text">
             <div class="content-page-title">
                 Seite nicht gefunden
             </div>
             <div>
                 Die von Ihnen aufgerufene Seite konnte leider nicht gefunden werden
             </div>
+        </div>
+        <div class="error-image-wrapper">
             <img
                 src="{{asset("/assets/resources/img/error.png")}}"
                 alt="Ein kleiner Zwerg"
-                style="
-                /*border: 1px solid red;*/
-                width:30%;
-                height:100%;
-                object-fit: cover;
-                overflow: hidden;
-             "
             >
         </div>
     </div>
