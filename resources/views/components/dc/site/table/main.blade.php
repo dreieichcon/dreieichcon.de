d15181f96c0213c57faaac56dcc690a9ede63b00
Demolite <tobi.brinke@gmail.com>
2024-10-31 12:42:35 +0100

    Programm-Table

--- resources/views/components/dc/site/table/main.blade.php
+++ resources/views/components/dc/site/table/main.blade.php
@@ -6,12 +6,15 @@
 <div class="table-stack">
     <table class="table-main">
         <thead class="table-header">
-        <tr>
-            {!! $head !!}
-        </tr>
+            <tr>
+                {!! $head !!}
+            </tr>
         </thead>
         <tbody class="table-body">
             {!! $body !!}
         </tbody>
     </table>
+    <div class="table-mobile">
+        {!! $mobile !!}
+    </div>
 </div>
